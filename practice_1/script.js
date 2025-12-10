f9f47333ce5dcdbe0b44224307793b90556ea129
Sc0u1 <KSHABURISHVILI@SEU.EDU.GE>
2025-12-10 23:19:09 +0400

    commit

---
+++ practice_1/script.js
@@ -0,0 +1,140 @@
+/* 
+function fn(){
+  console.log(5+5)
+}
+
+fn()
+
+function fn_1(a,b){
+  a = 1 
+  b = 5
+  console.log(a+b)
+}
+
+fn_1()
+
+function fn_2(a,b,c){
+  console.log(a+b-c)
+}
+
+fn_2(5,3,1)
+
+//////////////////////////
+function sum(a, b) {
+  return a + b
+}
+
+let x = sum(5, 3)
+console.log(x)
+//////////////////////////
+
+function fn_3(a, b, c, d) {
+  if (a > 2) {
+    return a - c
+  } else {
+    return "a is too small"
+  }
+}
+
+console.log(fn_3(1, 2, 3, 4))
+*/
+
+/*
+var a;
+var myAge = 19;
+a = myAge;
+
+console.log(a)
+*/
+
+/* 
+var a = 5;
+var b = 10;
+var c = "i am a";
+
+a = a+1;
+b = b+5;
+c = c+"string!";
+
+console.log(a)
+console.log(b)
+console.log(c)
+*/
+
+/*
+var studlyCapVar; 
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "a string";
+titleCaseOver = 9000;
+
+console.log(
+  studlyCapVar,
+  properCamelCase,
+  titleCaseOver
+)
+*/
+
+//var sum = 10+10;
+//console.log(sum)
+
+// (- + * /)
+
+/*
+var myVar = 11;
+myVar--
+
+console.log(myVar);
+*/
+
+/* 
+var pi;
+var a;
+a = 3;
+pi = 3.1415926535;
+
+t = pi-a
+c = pi*a
+o = a/pi
+console.log(t,c,o)
+*/
+
+/* 
+var remainder;
+var b;
+
+function fn(a){
+  if(a % 2 !==0) {
+    return "kenti";
+  } else {
+    return "luwi";
+  }
+}
+
+b=4
+remainder = b % 3;
+
+console.log(fn(b))
+*/
+////---------------------/////
+/* 
+var a=3;
+var b=17;
+var c=12;
+//
+
+a = a+7; // same as a+=7
+b/=3 // we can use whatever we want before "=" for example "+, -, *, /"
+c*=11
+
+console.log(a)
+console.log(b)
+console.log(c)
+*/
+
+
+
+
+
