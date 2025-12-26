59c4beeb6afd6d7007be97903de715ca2df5a8d6
Sc0u1 <KSHABURISHVILI@SEU.EDU.GE>
2025-12-26 22:03:04 +0400

    commit

---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,70 @@
+//1.aritmetic operators
+
+let x;
+x = 5+5;
+x = 5-5;
+x = 5*5;
+x = 5/5;
+x = 7%5;
+
+//concatenation
+x = 'hello'+'_'+'world';
+
+//exponent
+x = 5**2;
+//increment
+x = 1;
+//same as x = x+1 but shorter
+x++;
+//decrement
+//works same as increment
+x--;
+//x = x-1
+
+//assigment operator
+
+x= 10;
+x +=5;
+
+// same as x = x+5
+//worls with other operatores too
+
+x = 20
+x-=10
+
+x = 20
+x/=2
+
+
+x= 20
+x**=2
+
+
+
+//3. comparison operators 
+
+x = 2 == '2';
+//return is true bc  double == dont care what type it is only chacks if it is same 
+
+x = 2 === '2';
+//return is false bc it chack types and bc 2 and '2' isnt same type one is number and second is string it returns false
+//---------------------
+
+x = 2 != 3;
+//return is true bc 3 inst same as 2
+x = 2 != 2;
+//false bc 2 = 2
+x = 2 !== '2';
+//return is true bc '2' and 2 isnt same
+//--------------------
+
+x = 10 > 5
+x = 10 < 5
+x = 10 <= 5
+x = 10 >= 5
+
+
+
+
+
+console.log(x);
