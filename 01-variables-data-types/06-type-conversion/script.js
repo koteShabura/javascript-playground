7a38ba22015c29cf060c2aa7e64caa85bca913ab
Sc0u1 <KSHABURISHVILI@SEU.EDU.GE>
2025-12-26 21:42:00 +0400

    commit

--- 01-variables-data-types/06-type-conversion/script.js
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -1,1 +1,55 @@
 console.log('hello')
+
+//let amount  = '100';
+
+// change string to number 
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+// console.log(amount, typeof amount);
+
+
+//------------------
+
+
+//changing number into string
+
+// let amount = 100;
+// amount = amount.toString();
+
+// let x = amount + '00'
+// console.log(x ,typeof x)
+
+
+//------------------
+//change string to decimal
+
+// let amount = '99.9';
+// amount = parseFloat(amount);
+// console.log(amount, typeof amount);
+
+//------------------
+//convert number to boolean
+
+let amount = 1;
+amount = Boolean(amount);
+console.log(amount, typeof amount)
+//true if amount = 0 than false 
+
+
+//what if -->
+let x = 'hello';
+x = parseInt(x);
+console.log(x)
+//NaN aka not a number
+// other ways to get NaN-->
+console.log(Math.sqrt(-1))
+console.log(1 + NaN)
+console.log(undefined + undefined)
+console.log('one' / 3)
+//------------------
+
+
+//------------------
+//------------------
+//------------------
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # javascript-playground
-
+#practicing from yt video https://www.youtube.com/watch?v=BI1o2H9z9fo&t=4304s
+#name: Modern JavaScript From The Beginning | First 12 Hours
