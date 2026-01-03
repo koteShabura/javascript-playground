42e02c4546a746de206fc5040c62d45a2285be6b
Sc0u1 <KSHABURISHVILI@SEU.EDU.GE>
2026-01-03 19:06:10 +0400

    commit

---
+++ 01-variables-data-types/09-working-with-strings/script.js
@@ -0,0 +1,35 @@
+let x;
+
+const name = 'john';
+const age = 31;
+
+x = 'hello, my name is ' + name + ' and my age is '+ age
+console.log(x)
+
+// Template Literals
+x = `hello, my name is ${name} and my age is ${age}`
+
+
+// string properties and methods 
+
+const s = 'hello World'
+
+x = s.length;
+
+//acces value by kay
+x = s[1];
+
+x = s.__proto__;
+console.log(x)
+x = s.toUpperCase();;
+
+x = s.charAt(0) // same as x = s[1];
+
+x = s.indexOf('d')
+
+
+
+
+
+console.log(x)
+
