bfca4351cd40273d21da2783e84be90dd3846125
Sc0u1 <KSHABURISHVILI@SEU.EDU.GE>
2026-01-29 21:08:39 +0400

    commit

---
+++ sandBox/sandBox.js
@@ -0,0 +1,35 @@
+/*console.log('hello');
+
+let x = 4;
+
+function fn1() {
+  if (typeof x === "number") {
+    return "number";
+  } else {
+    return "not number";
+  }
+}
+
+console.log(fn1());
+
+function checkType(value) {
+  if (typeof value === "number") {
+    return "number";
+  }
+  return "not number";
+}
+
+console.log(checkType(4));
+console.log(checkType("4"));
+
+
+
+function checkType(value) {
+  return typeof value;
+}
+
+console.log(checkType(4));      // "number"
+console.log(checkType("a"));    // "string"
+console.log(checkType(true));   // "boolean"
+*/
+
